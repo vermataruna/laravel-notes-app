2bf0c9488893a02305bb38d92b346441152160f6
vermataruna <tarunaverma792@gmail.com>
2024-03-25 20:21:26 +0530

    add: note factory and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+Route::resource('note', NoteController::class);
